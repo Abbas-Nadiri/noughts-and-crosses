d21a62dadd8a9842aa033d94a11c865a323d7a3a
ahnadiri <108028111+Abbas-Nadiri@users.noreply.github.com>
2024-09-27 17:03:47 +0100

    create Player factory function

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-alert("meow")
+function Gameboard() {
+    gameboard = [];
+
+}
+
+let counter = 0; 
+function Player(name, symbol) {
+    if (counter == 0) {
+        symbol = "X";
+    } else {
+        symbol = "O"
+    };
+    counter++;
+    return {name, symbol};
+}
+
+console.log(Player("sigma thomas shelby"), Player("new yogurt male"));
