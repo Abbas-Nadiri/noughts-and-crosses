af8130a4f71ccccc25ec5e570e49a999b9d04062
ahnadiri <108028111+Abbas-Nadiri@users.noreply.github.com>
2024-10-02 08:21:05 +0100

    create PlayGame function and create logic that plays a round

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
-function Gameboard() {
-    gameboard = [];
-
+function Gameboard(symbol, move) {
+    let gameboard = [null, null, null, null, null, null, null, null, null];
+    gameboard.splice(move - 1, 1, symbol);
+    console.log(gameboard);
 }
 
 let counter = 0; 
 function Player(name, symbol) {
-    if (counter == 0) {
-        symbol = "X";
-    } else {
-        symbol = "O"
-    };
-    counter++;
-    return {name, symbol};
+    while (counter < 2) {
+        if (counter == 0) {
+            symbol = "X";
+        } else {
+            symbol = "O"
+        };
+        counter++;
+        return {name, symbol};
+    }
 }
 
-console.log(Player("sigma thomas shelby"), Player("new yogurt male"));
+function playGame() {
+    let p1 = Player(prompt("Player One Name:"));
+    let p2 = Player(prompt("Player Two Name:"));
+    console.log(p1, p2);
+
+    turnCounter = 0;
+    if (turnCounter % 2 == 0) {
+        let move = Number(prompt("Enter board position to place counter 1-9:"));
+        Gameboard(p1.symbol, move);
+    }
+}
+
+playGame();
